737c2f12924dc7999c43fc70a00d46110d5e6db8
Aleksandr Kuperman <aleksandr.kuperman@valory.xyz>
2023-06-10 14:08:51 +0100

    chore: updating the deployment script

--- scripts/deployment/deploy_01_grow_registry.js
+++ scripts/deployment/deploy_01_grow_registry.js
@@ -1,46 +1,18 @@
 /*global process*/
 
 const { ethers } = require("hardhat");
-const { LedgerSigner } = require("@anders-t/ethers-ledger");
 
 async function main() {
     const fs = require("fs");
     const globalsFile = "globals.json";
     const dataFromJSON = fs.readFileSync(globalsFile, "utf8");
     let parsedData = JSON.parse(dataFromJSON);
-    const derivationPath = parsedData.derivationPath;
     const providerName = parsedData.providerName;
     const gasPriceInGwei = parsedData.gasPriceInGwei;
     const growRegistryName = parsedData.growRegistryName;
     const growRegistrySymbol = parsedData.growRegistrySymbol;
     const baseURI = parsedData.baseURI;
 
-    let networkURL;
-    if (providerName === "polygon") {
-        if (!process.env.ALCHEMY_API_KEY_MATIC) {
-            console.log("set ALCHEMY_API_KEY_MATIC env variable");
-        }
-        networkURL = "https://polygon-mainnet.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY_MATIC;
-    } else if (providerName === "polygonMumbai") {
-        if (!process.env.ALCHEMY_API_KEY_MUMBAI) {
-            console.log("set ALCHEMY_API_KEY_MUMBAI env variable");
-            return;
-        }
-        networkURL = "https://polygon-mumbai.g.alchemy.com/v2/" + process.env.ALCHEMY_API_KEY_MUMBAI;
-    } else if (providerName === "gnosis") {
-        if (!process.env.GNOSIS_CHAIN_API_KEY) {
-            console.log("set GNOSIS_CHAIN_API_KEY env variable");
-            return;
-        }
-        networkURL = "https://rpc.gnosischain.com";
-    } else if (providerName === "chiado") {
-        networkURL = "https://rpc.chiadochain.net";
-    } else {
-        console.log("Unknown network provider", providerName);
-        return;
-    }
-
-    const provider = new ethers.providers.JsonRpcProvider(networkURL);
     const signers = await ethers.getSigners();
     const EOA = signers[0];
 
