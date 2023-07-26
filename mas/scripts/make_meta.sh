c588b014ec55e4061e9815f90121dc164810393b
8baller <tomrae030@hotmail.com>
2023-07-26 09:54:52 +0100

    [feat]fixes and descriptions added

--- mas/scripts/make_meta.sh
+++ mas/scripts/make_meta.sh
@@ -1,6 +1,6 @@
-python ~/Fun/auto_dev/scripts/metadata.py . service/eightballer/plantation_station/0.1.0 05
-python ~/Fun/auto_dev/scripts/metadata.py . agent/zarathustra/plantation/0.1.0 04
-python ~/Fun/auto_dev/scripts/metadata.py . skill/zarathustra/plantation_station_abci/0.1.0 03
-python ~/Fun/auto_dev/scripts/metadata.py . skill/zarathustra/plantation_abci/0.1.0 02
-python ~/Fun/auto_dev/scripts/metadata.py . contract/zarathustra/grow_registry/0.1.0 01
+python ~/Desktop/Fun/auto_dev/scripts/metadata.py . service/eightballer/plantation_station/0.1.0 05
+python ~/Desktop/Fun/auto_dev/scripts/metadata.py . agent/zarathustra/plantation/0.1.0 04
+python ~/Desktop/Fun/auto_dev/scripts/metadata.py . skill/zarathustra/plantation_station_abci/0.1.0 03
+python ~/Desktop/Fun/auto_dev/scripts/metadata.py . skill/zarathustra/plantation_abci/0.1.0 02
+python ~/Desktop/Fun/auto_dev/scripts/metadata.py . contract/zarathustra/grow_registry/0.1.0 01
 
